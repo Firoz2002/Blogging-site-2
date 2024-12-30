9065b417a741b8c6214dc9499861657f0f0e3288
FirozKamdar <firozkamdar01@gmail.com>
2024-12-30 12:01:17 +0530

    frontend work

---
+++ src/components/cards/Card.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <article className="text-center float-left mb-[110px] ps-12">
+      <figure>
+        <img src="" alt="" />
+      </figure>
+      <span>
+
+      </span>
+      <h2>
+
+      </h2>
+      <span>
+        
+      </span>
+    </article>
+  )
+}
